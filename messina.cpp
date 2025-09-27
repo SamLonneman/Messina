8f865dd4014d714a55555ab7add249203dd797ba
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2025-09-27 17:36:53 -0400

    First successful pitch detection

--- messina.cpp
+++ messina.cpp
@@ -1,42 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <climits>
 #include "AudioFile.h"
 
 
-long long df(int16_t* samples, int window_size, int lag)
+long long df(int16_t* samples, int windowSize, int lag)
 {
     long long sum = 0;
-    for (int j = 0; j < window_size; j++)
+    for (int j = 1; j <= windowSize; j++)
     {
-        sum += (samples[j] - samples[j + lag]) * (samples[j] - samples[j + lag]);
+        sum += pow(samples[j] - samples[j + lag], 2);
     }
     return sum;
 }
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-
+    // Load audio file and print summary
     AudioFile<int16_t> audioFile;
     audioFile.load("../audio/a440.wav");
     audioFile.printSummary();
 
-    int channel = 0;
+    // Get stats
     int numSamples = audioFile.getNumSamplesPerChannel();
     int sampleRate = audioFile.getSampleRate();
-    for (int i = 0; i < 20; i++)
-    {
-        int16_t currentSample = audioFile.samples[channel][i];
-        std::cout << i << ": " << currentSample << std::endl;
-    }
-
-
+    
+    // Determine interesting lags and window size
+    int minFrequency = 135;                                      // Lowest sung note (Hz)
+    int maxFrequency = 1400;                                     // Highest sung note (Hz)
+    int minLag = sampleRate / maxFrequency;                      // Smallest period to check (samples, rounded down)
+    int maxLag = (sampleRate + minFrequency - 1) / minFrequency; // Largest period to check (samples, rounded up)
+    int windowSize = maxLag * 2.5;                               // Rule of thumb is to use window size at least twice maxLag.
+    
+    // Display lag and window size info
+    std::cout << "minLag: " << minLag << " samples, i.e. " << (double)minLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / minLag << " Hz" << std::endl;
+    std::cout << "maxLag: " << maxLag << " samples, i.e. " << (double)maxLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / maxLag << " Hz" << std::endl;
+    std::cout << "windowSize: " << windowSize << " samples, i.e. " << (double)windowSize * 1000 / sampleRate << "ms, i.e. 2.5x maxLag" << std::endl;
+    
+    // Estimate pitch at t = 0
     int candidate = 0;
-    int windowSize = 20000;
-    for (int lag = 0; lag < 500; lag++)
+    int channel = 0;
+    int optimalLag = -1;
+    long long minDF = LLONG_MAX;
+    for (int currentLag = minLag; currentLag < maxLag; currentLag++)
     {
-        std::cout << "lag: " << lag << ", df: " << df(audioFile.samples[channel].data() + candidate, windowSize, lag) << std::endl;
+        long long currentDF = df(audioFile.samples[channel].data() + candidate, windowSize, currentLag);
+        std::cout << "lag: " << currentLag << ", df: " << currentDF << std::endl;
+        if (currentDF < minDF)
+        {
+            minDF = currentDF;
+            optimalLag = currentLag;
+        }
     }
 
-    int period = 100;
-    std::cout << sampleRate/period << std::endl;
+    // Print result
+    double F_0 = (double)sampleRate / optimalLag;
+    std::cout << "Estimated F_0 at t = " << (double)candidate * 1000 / sampleRate << " ms: " << F_0 << std::endl;
+
 }
