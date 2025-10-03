dc0ad00d628627230a443315132513895a7d1bab
samlonneman <56888420+SamLonneman@users.noreply.github.com>
2025-10-03 17:23:16 -0400

    Refactored

--- messina.cpp
+++ messina.cpp
@@ -7,6 +7,16 @@
 #include "AudioFile.h"
 
 
+namespace {
+    int minFrequency;
+    int maxFrequency;
+    int numSamples;
+    int sampleRate;
+    int minLag;
+    int maxLag;
+    int windowSize;
+}
+
 void printVector(const std::vector<double>& vec)
 {
     std::cout << "[";
@@ -25,7 +35,7 @@ void printMatrix(const std::vector<std::vector<double>>& mat)
     }
 }
 
-std::vector<double> gaussianElimination(std::vector<std::vector<double>>& mat)
+std::vector<double> solveSystemOfEquations(std::vector<std::vector<double>>& mat)
 {
     // Get matrix dimensions
     int m = mat.size();
@@ -103,8 +113,24 @@ std::vector<double> gaussianElimination(std::vector<std::vector<double>>& mat)
 
 }
 
+double parabolicInterpolation(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+    if (y1 == y2 == y3)
+    {
+        return x2;
+    }
+    std::vector<std::vector<double>> systemOfEquations =
+    {
+        { std::pow(x1, 2), x1, 1, y1 },
+        { std::pow(x2, 2), x2, 1, y2 },
+        { std::pow(x3, 2), x3, 1, y3 }
+    };
+    std::vector<double> coefficients = solveSystemOfEquations(systemOfEquations);
+    return -coefficients[1] / (2 * coefficients[0]);
+}
+
 // Autocorrelation function
-long long acf(int16_t* samples, int windowSize, int lag)
+long long acf(int16_t* samples, int lag)
 {
     long long sum = 0;
     for (int j = 1; j <= windowSize; j++)
@@ -115,7 +141,7 @@ long long acf(int16_t* samples, int windowSize, int lag)
 }
 
 // Difference function v1 (intuitive form)
-long long dfv1(int16_t* samples, int windowSize, int lag)
+long long dfv1(int16_t* samples, int lag)
 {
     long long sum = 0;
     for (int j = 1; j <= windowSize; j++)
@@ -127,43 +153,19 @@ long long dfv1(int16_t* samples, int windowSize, int lag)
 }
 
 // Difference function v2 (in terms of ACF)
-long long dfv2(int16_t* samples, int windowSize, int lag)
+long long dfv2(int16_t* samples, int lag)
 {
-    return acf(samples, windowSize, 0) + acf(samples + lag, windowSize, 0) - 2 * acf(samples, windowSize, lag);
+    return acf(samples, 0) + acf(samples + lag, 0) - 2 * acf(samples, lag);
 }
 
-int main()
+double estimateF_0(int16_t* samples)
 {
-    // Load audio file and print summary
-    AudioFile<int16_t> audioFile;
-    audioFile.load("../audio/a440.wav");
-    audioFile.printSummary();
-
-    // Get stats
-    int numSamples = audioFile.getNumSamplesPerChannel();
-    int sampleRate = audioFile.getSampleRate();
-
-    // Determine lags of interest and window size
-    int minFrequency = 135;                                      // Lowest sung note (Hz)
-    int maxFrequency = 1400;                                     // Highest sung note (Hz)
-    int minLag = sampleRate / maxFrequency;                      // Smallest period to check (samples, rounded down)
-    int maxLag = (sampleRate + minFrequency - 1) / minFrequency; // Largest period to check (samples, rounded up)
-    int windowSize = maxLag * 2.5;                               // Rule of thumb is to use window size at least twice maxLag.
-
-    // Display lag and window size info
-    std::cout << "minLag: " << minLag << " samples, i.e. " << (double)minLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / minLag << " Hz" << std::endl;
-    std::cout << "maxLag: " << maxLag << " samples, i.e. " << (double)maxLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / maxLag << " Hz" << std::endl;
-    std::cout << "windowSize: " << windowSize << " samples, i.e. " << (double)windowSize * 1000 / sampleRate << "ms, i.e. 2.5x maxLag" << std::endl;
-
-    // Estimate pitch at t = 0
-    int candidate = 0;
-    int channel = 0;
+    // Linearly search lags to find the one with the smallest DF.
     int optimalLag = -1;
     long long minDF = LLONG_MAX;
-    int16_t* samples = audioFile.samples[channel].data();
     for (int currentLag = minLag; currentLag < maxLag; currentLag++)
     {
-        long long currentDF = dfv1(samples + candidate, windowSize, currentLag);
+        long long currentDF = dfv1(samples, currentLag);
         if (currentDF < minDF)
         {
             minDF = currentDF;
@@ -172,20 +174,46 @@ int main()
     }
 
     // Parabolic interpolation
-    long long priorDF = dfv1(samples + candidate, windowSize, optimalLag - 1);
-    long long optimalDF = dfv1(samples + candidate, windowSize, optimalLag);
-    long long nextDF = dfv1(samples + candidate, windowSize, optimalLag + 1);
-    std::vector<std::vector<double>> systemOfEquations =
-    {
-        { (double)((optimalLag - 1) * (optimalLag - 1)), (double)(optimalLag - 1), 1, (double)priorDF   },
-        { (double)((optimalLag    ) * (optimalLag    )), (double)(optimalLag    ), 1, (double)optimalDF },
-        { (double)((optimalLag + 1) * (optimalLag + 1)), (double)(optimalLag + 1), 1, (double)nextDF    }
-    };
-    std::vector<double> solution = gaussianElimination(systemOfEquations);
-    double interpolatedOptimalLag = -solution[1] / (2 * solution[0]);
+    long long priorDF = dfv1(samples, optimalLag - 1);
+    long long nextDF = dfv1(samples, optimalLag + 1);
+    double periodInSamples = parabolicInterpolation(optimalLag - 1, priorDF, optimalLag, minDF, optimalLag + 1, nextDF);
 
     // From interpolated optimal lag, calculate final F_0 estimate
-    double F_0 = sampleRate / interpolatedOptimalLag;
-    std::cout << "\nEstimated F_0 at t = " << (double)candidate * 1000 / sampleRate << " ms: " << F_0 << " Hz" << std::endl;
+    return sampleRate / periodInSamples;
+
+}
+
+int main()
+{
+    // Load audio file and print summary
+    AudioFile<int16_t> audioFile;
+    audioFile.load("../audio/a440.wav");
+    int16_t* samples = audioFile.samples[0].data();
+    
+    // Get audio file properties
+    numSamples = audioFile.getNumSamplesPerChannel();
+    sampleRate = audioFile.getSampleRate();
+    
+    // Determine lags of interest and window size
+    minFrequency = 135;                                      // Lowest sung note (Hz)
+    maxFrequency = 1400;                                     // Highest sung note (Hz)
+    minLag = sampleRate / maxFrequency;                      // Smallest period to check (samples, rounded down)
+    maxLag = (sampleRate + minFrequency - 1) / minFrequency; // Largest period to check (samples, rounded up)
+    windowSize = maxLag * 2.5;                               // Rule of thumb is to use window size at least twice maxLag.
+    
+    // Print relevant debug info
+    std::cout << std::endl;
+    audioFile.printSummary();
+    std::cout << "minLag: " << minLag << " samples, i.e. " << (double)minLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / minLag << " Hz" << std::endl;
+    std::cout << "maxLag: " << maxLag << " samples, i.e. " << (double)maxLag * 1000 / sampleRate << " ms, i.e. " << (double)sampleRate / maxLag << " Hz" << std::endl;
+    std::cout << "windowSize: " << windowSize << " samples, i.e. " << (double)windowSize * 1000 / sampleRate << " ms, i.e. 2.5x maxLag" << std::endl;
+    std::cout << "|======================================|" << std::endl << std::endl;
+
+    // Estimate pitch at first 100 samples
+    for (int i = 0; i < 100; i++)
+    {
+        double F_0 = estimateF_0(samples + i);
+        std::cout << "Estimated F_0 at sample " << i << " (t = " << (double)i * 1000 / sampleRate << " ms): " << F_0 << " Hz" << std::endl;
+    }
 
 }
